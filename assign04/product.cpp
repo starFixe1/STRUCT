d89978620b58d59c388c6c243a73870170bb3885
starFixe1 <bubba.fixe@outlook.com>
2019-05-15 19:47:50 -0400

    read

--- assign04/product.cpp
+++
@@ -1,9 +0,0 @@
-/***************************************************************
- * File: product.cpp
- * Author: (your name here)
- * Purpose: Contains the method implementations for the Product class.
- ***************************************************************/
-
-#include "product.h"
-
-// put your method bodies here
